1e7fe67138f79ef01bb5a0e924778adbd0af47ce
BigBossJacob <jacob.bonner@rogers.com>
2024-12-01 00:12:19 -0500

    Counter Interactable Makes Crank Sound

--- src/CounterInteractable.h
+++ src/CounterInteractable.h
@@ -13,6 +13,8 @@
 #include <godot_cpp/classes/sphere_mesh.hpp>
 #include <godot_cpp/classes/array_mesh.hpp>
 #include <godot_cpp/classes/texture2d.hpp>
+#include <godot_cpp/classes/audio_stream_player3d.hpp>
+#include <godot_cpp/classes/audio_stream_wav.hpp>
 
 #include <godot_cpp/variant/vector3.hpp>
 
@@ -29,6 +31,9 @@ private:
     // Counter member variables
     int counter;
     int trigger;
+
+    // Audio player for sound effects
+    AudioStreamPlayer3D* crank;
     
 protected:
     static void _bind_methods();
@@ -40,7 +45,8 @@ public:
     void _enter_tree ( ) override;
     void _ready ( ) override;
 
-    // This member function sets values for the counter and trigger
+    // This member function sets values for the counter, trigger, audio
+    void SetInit(int c, int t);
     void SetCounter(int c);
     void SetTrigger(int t);
 
@@ -49,6 +55,11 @@ public:
 
     // This member function is triggered when
     void Trigger();
+
+    // the return type represents whether it existed already; true if it is brand-new; false if it was retrieved from the SceneTree
+	// search defines whether the scenetree should be checked for an instance
+	template <class T>
+	bool create_and_add_as_sub_child(T *&pointer, String name, bool search = false);
 };
 
 }
